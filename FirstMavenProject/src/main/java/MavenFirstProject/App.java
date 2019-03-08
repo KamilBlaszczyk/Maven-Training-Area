a1f13373f6d30f078d3cf1fbf8a4aefda49e49ed
Kamil Blaszczyk <kejm03@gmail.com>
2019-03-08 11:20:18 +0100

    Create app.java

---
+++ FirstMavenProject/src/main/java/MavenFirstProject/App.java
@@ -0,0 +1,14 @@
+package MavenFirstProject;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
