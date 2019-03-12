4f50081f2d50b18002760dd2de3a905697809b38
Kamil Blaszczyk <kejm03@gmail.com>
2019-03-12 08:31:49 +0100

    Update App.java

--- FirstMavenProject/src/main/java/MavenFirstProject/App.java
+++ FirstMavenProject/src/main/java/MavenFirstProject/App.java
@@ -1,14 +1,27 @@
 package MavenFirstProject;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+/**
+ * Created by Blaszczyk Kamil
+ */
+// rodzaj beana (typu klasy zarządzanej przez Spring)
+@RestController
+public class App { // nazwa klasy
+
+    // wskazanie pod jakim adresem dostępna jest metoda
+    @RequestMapping("/hello")
+    // sygnatura metody
+    public String hello() {
+        // zwracana wartość przez przeglądarkę
+        return "Hello World! :)";
     }
+
+    @RequestMapping("/hey")
+    public String hey() {
+        // zwracana wartość przez przeglądarkę
+        return "Yeah :)";
+    }
+
 }
