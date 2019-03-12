3b4c8e4309c47d232ed79c6a19e2da6b6b331bc1
Kamil Blaszczyk <kejm03@gmail.com>
2019-03-12 14:58:32 +0100

    Create GoodPizza.java

---
+++ FirstMavenProject/src/main/java/implementation/GoodPizza.java
@@ -0,0 +1,26 @@
+package implementation;
+
+import api.IPizza;
+
+public class GoodPizza implements IPizza {
+
+    private int price;
+    private String name;
+
+    public GoodPizza(int price, String name) {
+        super();
+        this.price = price;
+        this.name = name;
+    }
+
+    @Override
+    public int getPrice() {
+        return price;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+}
