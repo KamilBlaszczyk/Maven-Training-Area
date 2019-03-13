c23d118c7cbe0e1359a3161bcc5360ed43b801ff
Kamil Blaszczyk <kejm03@gmail.com>
2019-03-13 12:26:27 +0100

    Create Config.java

---
+++ FirstMavenProject/src/main/java/config/Config.java
@@ -0,0 +1,23 @@
+package config;
+
+import api.IOrder;
+import api.IPizza;
+import implementation.Order;
+import implementation.SalamiPizza;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//@Configuration
+public class Config {
+
+//    @Bean
+//    public IPizza pizza(){
+//        return new SalamiPizza(15, "Pizza z Salami");
+//    }
+
+//    @Bean(name = "inna")
+//    public IOrder order(IPizza pizza){
+//        return new Order(pizza);
+//    }
+
+}
