3f687f62be335e4107f3ad50e9489678454f27d7
Kamil Blaszczyk <kejm03@gmail.com>
2019-03-13 12:27:22 +0100

    Update Test.java

--- FirstMavenProject/src/main/java/pizzeria/Test.java
+++ FirstMavenProject/src/main/java/pizzeria/Test.java
@@ -1,14 +1,17 @@
 package pizzeria;
 
 import api.IOrder;
+import config.AutoConfig;
+import config.Config;
 import implementation.Order;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Test {
 
     public static void main(String []args){
-        AnnotationConfigApplicationContext context  = new AnnotationConfigApplicationContext(Config.class);
-        IOrder order = context.getBean(Order.class);
+        AnnotationConfigApplicationContext context  = new AnnotationConfigApplicationContext(AutoConfig.class);
+        IOrder order = context.getBean(IOrder.class);
+
         order.printOrder();
     }
 
