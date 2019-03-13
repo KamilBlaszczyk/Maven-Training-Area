d1da5770740d27c3b36582fb3bc5c4b649e2c08b
Kamil Blaszczyk <kejm03@gmail.com>
2019-03-13 12:26:02 +0100

    Create Auto Configuration in Spring Boot Project

---
+++ FirstMavenProject/src/main/java/config/AutoConfig.java
@@ -0,0 +1,13 @@
+package config;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+
+@Configuration  //Ustawienie tej klasy jako klasy konfiguracyjnej
+@ComponentScan("implementation")  //Przeszukiwanie komponentów. W nawiasie paczka z klasami
+@Import(Config.class)  //Importowanie pliku konfiguracyjnego
+public class AutoConfig {
+}
