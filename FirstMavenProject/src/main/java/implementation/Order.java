408650de25d6949a68eb2e906f3ee16b197c4641
Kamil Blaszczyk <kejm03@gmail.com>
2019-03-13 12:26:56 +0100

    Changes in implementation

--- FirstMavenProject/src/main/java/implementation/Order.java
+++ FirstMavenProject/src/main/java/implementation/Order.java
@@ -2,17 +2,22 @@ package implementation;
 
 import api.IOrder;
 import api.IPizza;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 
+@Component
 public class Order implements IOrder {
 
     private IPizza pizza;
 
-    public Order(IPizza pizza) {
+    public Order(IPizza pizza) { //@Qualifier("dobra")
         super();
         this.pizza = pizza;
     }
 
     public void printOrder(){
-        System.out.println("zamowienie:" + pizza.getName() + " Cena: " + pizza.getPrice());
+        System.out.println("zamowienie: " + pizza.getName() + " Cena: " + pizza.getPrice());
     }
+
+
 }
