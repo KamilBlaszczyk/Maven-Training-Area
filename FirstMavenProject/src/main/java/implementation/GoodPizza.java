7b7efa499f0b2892b55e67f58fedd9e79bde963d
Kamil Blaszczyk <kejm03@gmail.com>
2019-03-13 12:26:59 +0100

    Update GoodPizza.java

--- FirstMavenProject/src/main/java/implementation/GoodPizza.java
+++ FirstMavenProject/src/main/java/implementation/GoodPizza.java
@@ -1,13 +1,20 @@
 package implementation;
 
 import api.IPizza;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
 
+@Component
+//@Qualifier("dobra")
+@Primary
 public class GoodPizza implements IPizza {
 
     private int price;
     private String name;
 
-    public GoodPizza(int price, String name) {
+    public GoodPizza(@Value("10") int price, @Value("good") String name) {
         super();
         this.price = price;
         this.name = name;
