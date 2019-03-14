fb8144e21a337da198f39cec307080830b9654fb
Kamil Blaszczyk <kejm03@gmail.com>
2019-03-14 13:46:14 +0100

    Create Oxfords.java

---
+++ Spring and Maven training/src/main/java/implementation/Oxfords.java
@@ -0,0 +1,32 @@
+package implementation;
+
+import api.ElasticSides;
+import api.IShoe;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+//@Primary
+public class Oxfords implements IShoe {
+
+    private  int price;
+    private String name;
+
+    public Oxfords(@Value("oxfordy") String name, @Value("250") int price) {
+        super();
+        this.name = name;
+        this.price = price;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int getPrice() {
+        return price;
+    }
+}
