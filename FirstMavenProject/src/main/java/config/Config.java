e5feb362c38a5cf90b9349187fa17266a6d05167
Kamil Blaszczyk <kejm03@gmail.com>
2019-03-16 09:09:59 +0100

    Update Config.java

--- FirstMavenProject/src/main/java/config/Config.java
+++ FirstMavenProject/src/main/java/config/Config.java
@@ -1,12 +1,5 @@
 package config;
 
-import api.IOrder;
-import api.IPizza;
-import implementation.Order;
-import implementation.SalamiPizza;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
 //@Configuration
 public class Config {
 
