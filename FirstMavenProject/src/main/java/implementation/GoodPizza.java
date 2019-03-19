cbb640eed748eb49d656854f6bdce1f5158e073e
Kamil Blaszczyk <kejm03@gmail.com>
2019-03-19 22:47:30 +0100

    Update GoodPizza.java

--- FirstMavenProject/src/main/java/implementation/GoodPizza.java
+++ FirstMavenProject/src/main/java/implementation/GoodPizza.java
@@ -3,20 +3,34 @@ package implementation;
 import api.IPizza;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-@Component
 //@Qualifier("dobra")
+@Component
 @Primary
+@PropertySource("classpath:prices.properties")
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class GoodPizza implements IPizza {
 
     private int price;
     private String name;
 
-    public GoodPizza(@Value("15") int price, @Value("good") String name) {
+    public GoodPizza(@Value("${price_good}") int price, @Value("${name}") String name) {
         super();
         this.price = price;
+        this.name = name;
+    }
+
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setName(String name) {
         this.name = name;
     }
 
