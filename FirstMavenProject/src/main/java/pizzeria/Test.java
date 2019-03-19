71060af91e8061adefe8d4d91c07e2807a401c09
Kamil Blaszczyk <kejm03@gmail.com>
2019-03-19 22:48:03 +0100

    Update Test.java

--- FirstMavenProject/src/main/java/pizzeria/Test.java
+++ FirstMavenProject/src/main/java/pizzeria/Test.java
@@ -10,9 +10,12 @@ public class Test {
 
     public static void main(String []args){
         AnnotationConfigApplicationContext context  = new AnnotationConfigApplicationContext(AutoConfig.class);
+
         IOrder order = context.getBean(IOrder.class);
+        IOrder order2 = context.getBean(Order.class);
 
         order.printOrder();
+        order2.printOrder();
     }
 
 }
