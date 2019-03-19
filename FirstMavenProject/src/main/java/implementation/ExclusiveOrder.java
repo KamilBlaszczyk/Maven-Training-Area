6c03c55b45b0b1c1c7a7045426f7b238c4d66bce
Kamil Blaszczyk <kejm03@gmail.com>
2019-03-19 22:47:42 +0100

    Update ExclusiveOrder.java

--- FirstMavenProject/src/main/java/implementation/ExclusiveOrder.java
+++ FirstMavenProject/src/main/java/implementation/ExclusiveOrder.java
@@ -4,8 +4,11 @@ import api.GoodPizza;
 import api.IOrder;
 import api.IPizza;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @Primary
@@ -13,7 +16,11 @@ public class ExclusiveOrder implements IOrder {
 
 //    @Autowired //Wstrzykiwanie przez Refleksje
 //    @GoodPizza
+
+    @Value("#{listOfPizza[1]}")
     private IPizza pizza;
+    @Autowired
+    private List<IPizza> listOfPizza;
 
     public ExclusiveOrder() {
         super();
@@ -23,8 +30,11 @@ public class ExclusiveOrder implements IOrder {
         System.out.println("zamowienie exclusive: " + pizza.getName() + " Cena: " + pizza.getPrice());
     }
 
-    @Autowired
-    @GoodPizza
+    public IPizza getPizza() {
+        return pizza;
+    }
+
+//    @Value("#{exoticPizza}")   //SpEL used inject be default ID class
     public void setPizza(IPizza pizza) {
         this.pizza = pizza;
     }
