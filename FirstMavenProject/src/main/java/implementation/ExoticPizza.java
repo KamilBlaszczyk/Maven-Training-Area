f2ae678a0528c5967780284d505337525093e1a7
Kamil Blaszczyk <kejm03@gmail.com>
2019-03-19 22:47:48 +0100

    Update ExoticPizza.java

--- FirstMavenProject/src/main/java/implementation/ExoticPizza.java
+++ FirstMavenProject/src/main/java/implementation/ExoticPizza.java
@@ -16,14 +16,26 @@ public class ExoticPizza implements IPizza {
     private int price;
     private String name;
 
-    public ExoticPizza(@Value("1") int price, @Value("Exotic") String name) {
+    @Value("#{goodPizza.price matches '[1-9][1-9]'}")
+    boolean bool;
+
+    public ExoticPizza(@Value("#{goodPizza.price > 13 ? 13 : 20}") int price, @Value("#{goodPizza.getName()?.toUpperCase()}") String name) {
         super();
         this.price = price;
         this.name = name;
     }
 
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     @Override
     public int getPrice() {
+        System.out.println(bool);
         return price;
     }
 
@@ -31,4 +43,25 @@ public class ExoticPizza implements IPizza {
     public String getName() {
         return name;
     }
+
+
+
+
+
+
+
+
+
+
+
+//        SpEL
+//        @Value("#{goodPizza.getPrice()}")
+//        @Value("#{goodPizza.getName()?.toUpperCase()}")
+//        @Value("#{T(java.lang.Math).random()*10}")
+//        @Value("#{ goodPizza.price <13 ? 13 : 20}")
+
+
+
+
+
 }
