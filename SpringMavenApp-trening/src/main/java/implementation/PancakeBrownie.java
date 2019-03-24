6f1e627c90c2b7718c4ce740d4fe7c9b8ff6f25b
Kamil Blaszczyk <kejm03@gmail.com>
2019-03-24 21:50:52 +0100

    Update PancakeBrownie.java

--- SpringMavenApp-trening/src/main/java/implementation/PancakeBrownie.java
+++ SpringMavenApp-trening/src/main/java/implementation/PancakeBrownie.java
@@ -1,13 +1,23 @@
 package implementation;
 
 import api.IPancake;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
+@Component
+@Primary
+@PropertySource("classpath:prices.properties")
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class PancakeBrownie implements IPancake {
 
     private int price;
     private String name;
 
-    public PancakeBrownie(int price, String name){
+    public PancakeBrownie(@Value("${price}") int price, @Value("${name}") String name){
         super();
         this.price = price;
         this.name = name;
