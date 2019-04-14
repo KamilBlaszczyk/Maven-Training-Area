02f020b0ab87cdf185f2efd6d43000a0feafe132
Kamil Blaszczyk <blaszczyk.kamil.piotr@gmail.com>
2019-04-14 23:19:53 +0100

    Create Ship.java

---
+++ Maven Project/src/main/java/com/blaszczyk/implementation/Ship.java
@@ -0,0 +1,11 @@
+package com.blaszczyk.implementation;
+
+import com.blaszczyk.api.Vehicle;
+
+public class Ship extends Vehicle {
+
+    public Ship(String manufacturer, int maxSpeed) {
+        super(manufacturer, maxSpeed);
+    }
+
+}
